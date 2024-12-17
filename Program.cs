5bfedc3fa6414e27554308959947ddd82aa58084
Kab-35-16 <Kab-35-16@KAB-31-7->
2024-12-17 11:15:25 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> zodiacSigns = new List<string>();
+
+            zodiacSigns.Add("овен");
+            zodiacSigns.Add("телец");
+            zodiacSigns.Add("рак");
+
+            Console.WriteLine("Исходный список:");
+            OutputList(zodiacSigns);
+
+            for (int i = 0; i < zodiacSigns.Count; i++)
+            {
+                zodiacSigns[i] = zodiacSigns[i].ToUpper();
+            }
+
+            Console.WriteLine("\nСписок в верхнем регистре:");
+            OutputList(zodiacSigns);
+
+            Console.WriteLine($"\nКоличество элементов: {zodiacSigns.Count}");
+
+            zodiacSigns.Add("новый знак");
+            Console.WriteLine("\nДобавлен элемент в конец:");
+            OutputList(zodiacSigns);
+        }
+        static void OutputList(List<string> list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
